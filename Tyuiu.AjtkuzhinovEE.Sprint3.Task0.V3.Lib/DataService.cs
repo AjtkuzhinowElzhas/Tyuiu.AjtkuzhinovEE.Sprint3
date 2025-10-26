6cd35b4a359263db6c0963ec5f86b9ce03a0742b
AjtkuzhinovElzhas <aitkuzinow8@gmail.com>
2025-10-26 14:09:02 +0300

    Task0

--- Tyuiu.AjtkuzhinovEE.Sprint3.Task0.V3.Lib/DataService.cs
+++ Tyuiu.AjtkuzhinovEE.Sprint3.Task0.V3.Lib/DataService.cs
@@ -1,6 +1,18 @@
 ﻿namespace Tyuiu.AjtkuzhinovEE.Sprint3.Task0.V3.Lib;
+using tyuiu.cources.programming.interfaces.Sprint3;
 
-public class Class1
+public class DataService : ISprint3Task0V3
 {
+    public double GetSumSeries(int startValue, int stopValue)
+    {
+        DataService ds = new DataService();
 
+        double sumSeries = 0;
+        int i;
+        for (i = startValue; i <= stopValue; i++)
+        {
+            sumSeries += Math.Sin(i) * Math.Pow(0.5, 2);
+        }
+        return Math.Round(sumSeries, 3);
+    }
 }
