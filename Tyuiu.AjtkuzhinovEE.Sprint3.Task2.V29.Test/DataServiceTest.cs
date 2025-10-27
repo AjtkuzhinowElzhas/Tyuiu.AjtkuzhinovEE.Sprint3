c4c92dc1dc4f2f9660a269fb5f2b6f922bd177c3
AjtkuzhinovElzhas <aitkuzinow8@gmail.com>
2025-10-27 14:58:07 +0300

    Task2

---
+++ Tyuiu.AjtkuzhinovEE.Sprint3.Task2.V29.Test/DataServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using Tyuiu.AjtkuzhinovEE.Sprint3.Task2.V29.Lib;
+
+namespace Tyuiu.AjtkuzhinovEE.Sprint3.Task2.V29.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetSumSeries()
+        {
+            double value = 0.5;
+            int startValue = 1;
+            int stopValue = 18;
+
+            DataService ds = new DataService();
+
+            double res = ds.GetSumSeries(value, startValue, stopValue);
+            double wait = -0.186;
+            Assert.AreEqual(wait, res);
+
+
+        }
+    }
+}
---
+++ Tyuiu.AjtkuzhinovEE.Sprint3.Task2.V29/Program.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.AjtkuzhinovEE.Sprint3.Task2.V29.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+
+        Console.WriteLine("*Написать программу используя цикл do-while,которая вычисляет сумму ряда по*");
+        Console.WriteLine("*формуле , при а = 0.5                                                    *");
+
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        double value = 0.5;
+        int startValue = 1; // старт
+        int stopValue = 18; // конец
+
+        Console.WriteLine("Переменная а = " + value);
+        Console.WriteLine("Старт шага = " + startValue);
+        Console.WriteLine("Конец шага = " + stopValue);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        DataService ds = new DataService();
+
+
+
+        Console.WriteLine("Сумма ряда = " + ds.GetSumSeries(value, startValue, stopValue));
+        Console.ReadKey();
+    }
+}
