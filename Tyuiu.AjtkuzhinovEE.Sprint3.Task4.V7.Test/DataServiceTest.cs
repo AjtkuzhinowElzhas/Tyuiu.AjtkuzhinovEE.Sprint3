441716747fee5619717d9a2674fb942728ae88da
AjtkuzhinovElzhas <aitkuzinow8@gmail.com>
2025-10-30 12:31:25 +0300

    Task4

---
+++ Tyuiu.AjtkuzhinovEE.Sprint3.Task4.V7.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.AjtkuzhinovEE.Sprint3.Task4.V7.Lib;
+
+namespace Tyuiu.AjtkuzhinovEE.Sprint3.Task4.V7.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+              DataService ds = new DataService();
+
+            int startValue = -5;
+            int stopValue = 5;
+            double res = ds.Calculate(startValue, stopValue);
+            double wait = 1231.499;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
