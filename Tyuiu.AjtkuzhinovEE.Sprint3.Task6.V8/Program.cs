161f577d661677f8dbd9757850db399e2ecb6bfd
AjtkuzhinovElzhas <aitkuzinow8@gmail.com>
2025-10-31 17:56:47 +0300

    Task6

---
+++ Tyuiu.AjtkuzhinovEE.Sprint3.Task6.V8/Program.cs
@@ -0,0 +1,24 @@
+﻿using Tyuiu.AjtkuzhinovEE.Sprint3.Task6.V8.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        int startValue = 18;
+        int stopValue = 28;
+
+        Console.WriteLine("Старт шага:" + startValue);
+        Console.WriteLine("Конец шага:" + stopValue);
+
+
+
+        Console.WriteLine("********************************************************************************");
+        Console.WriteLine("* Результат:                                                                   *");
+        Console.WriteLine("********************************************************************************");
+
+
+        Console.WriteLine("Сумма  = " + ds.GetSumTheDivisors(startValue, stopValue));
+
+        Console.ReadKey();
+    }
+}
