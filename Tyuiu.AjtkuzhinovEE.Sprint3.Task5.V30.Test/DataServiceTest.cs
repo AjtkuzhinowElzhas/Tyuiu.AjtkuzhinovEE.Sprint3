fbad61583e22d274261f79bdb967538eb615b472
AjtkuzhinovElzhas <aitkuzinow8@gmail.com>
2025-10-31 17:26:37 +0300

    Task5

---
+++ Tyuiu.AjtkuzhinovEE.Sprint3.Task5.V30.Test/DataServiceTest.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.AjtkuzhinovEE.Sprint3.Task5.V30.Lib;
+
+namespace Tyuiu.AjtkuzhinovEE.Sprint3.Task5.V30.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
